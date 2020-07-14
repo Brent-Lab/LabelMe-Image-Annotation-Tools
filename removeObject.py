6c2a96cf656eb89550dbc3d9cac04844da05d2e5
emmazucati <emmazucati@gmail.com>
2020-07-14 15:51:16 -0700

    currently only runs with one file at a time

--- removeObject.py
+++ removeObject.py
@@ -1,29 +1,90 @@
 #remove_object but python
 import json
 import sys
-import cv2 as cv
+import cv2
 import numpy as np
+import os
+import utilities
 
-if len(sys.argv) != 4:
-    print("Correct format is:")
-    print("python removeObject.py JSONfileName JPGfileName objectToRemove")
-else:
-    jsonFileName = sys.argv[1]
-    jpgFileName = sys.argv[2]
-    objectName = sys.argv[3]
-    #open file
-    openFile = open(jsonFileName, "r")
-    pyJson = json.load(openFile)
-    openFile.close()
-    #pull points for object to change
-    #list of dicts
-    shapesDict = pyJson['shapes']
-    #list of Dicts with label:objectName   
-    objectDict = [d for d in shapesDict if objectName in d.values()]
-    for d in objectDict:
-        #list of coordinates [x,y]
-        listPoints = d['points']
-        npPoints = np.array([listPoints], dtype=np.int32)
-        img = cv.imread(jpgFileName, cv.IMREAD_COLOR)
-        #listPoints needs to be a numpy array
-        cv.fillPoly(img, npPoints, (255, 255, 255)) 
+if __name__ == '__main__':
+
+    src_folder_name = sys.argv[1]
+    item_name = sys.argv[2]
+    allowed_image_types = (".jpg", ".jpeg")
+    work_dir = os.getcwd()
+    img_file_count = 0
+    
+    #TODO: traverse folder and open each file individually
+    
+    src_folder_dir = os.path.join(work_dir, src_folder_name)
+    src_folder_dir = os.path.join(src_folder_dir, '')
+    if not os.path.exists(src_folder_dir):
+        print(src_folder_dir + " does not exist")
+    else:   
+    
+        dest_folder_dir = utilities.create_folder(work_dir, "Renamed " + os.path.basename(os.path.dirname(src_folder_dir)))
+    
+        for filename in os.listdir(src_folder_dir):
+            name, ext = os.path.splitext(os.path.join(src_folder_dir, filename))
+            name, ext = name.lower(), ext.lower()
+            #since json and jpg files have same names, once you have jpeg, you also have json
+            #so filename = jpg_file_name
+            if ext.endswith(allowed_image_types):
+                img = None
+                img_file_count += 1
+                json_filename = name + ".json"
+                # Copy image #
+                new_name = item_name + "_" + str(img_file_count)
+                new_img_name = new_name + ext
+                new_json_filename = new_name + ".json" 
+    
+                #open file
+                open_file = open(json_filename, "r")
+                py_json = json.load(open_file)
+                #pull points for object to change
+                #list of dicts
+                shapes_dict = py_json['shapes']
+                #list of Dicts with label:objectName   
+                object_dict = [d for d in shapes_dict if item_name in d.values()]
+                print(len(object_dict))
+                for d in object_dict:
+                    #list of coordinates [x,y]
+                    #list_points needs to be a numpy array so now np_points
+                    list_points = d['points']
+                    np_points = np.array([list_points], dtype=np.int32)
+                    img = cv2.imread(filename, cv2.IMREAD_COLOR)
+                    cv2.fillPoly(img, np_points, (255, 255, 255)) 
+                #img is edited og
+
+                #jpeg done
+                if len(object_dict) > 0:
+                    while True:
+                        #scaled_width = w*scale
+                        #scaled_height = h*scale
+                        #cv2.imshow("Display", cv2.resize(img, (scaled_width, scaled_height)))
+                        
+                        #hard coded to fit screen
+                        cv2.imshow("Display", cv2.resize(img, (960, 540)))
+                        k = cv2.waitKey(1)  # This pegs my CPU. 
+                        if k == 27: #esc
+                            break
+                    cv2.destroyAllWindows()
+                    utilities.cv2_copy_and_rename_file(src_folder_dir, dest_folder_dir, filename, new_img_name, img)
+                else:
+                    utilities.copy_and_rename_file(src_folder_dir, dest_folder_dir, filename, new_img_name)
+                if not os.path.exists(os.path.join(src_folder_dir, json_filename)):
+                    continue
+    
+                #editing json file
+                for element in py_json['shapes']:
+                    #element and py_json are both dictionary
+                    if item_name in element.values():
+                        #remove the dictionary element from the list py_json['shapes']
+                        py_json['shapes'].remove(element) 
+                #py_json is new json file          
+                open_file.close()
+                #json done
+                new_json_dir = os.path.join(dest_folder_dir, new_json_filename)
+                with open(new_json_dir, 'w') as data_file:
+                    py_json = json.dump(py_json, data_file)
+                print("Finished editing " + str(img_file_count) + " images and corresponding json files.")
